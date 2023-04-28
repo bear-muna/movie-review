9aab646b82dc5314d7871a48af6f7a867beb0020
bear-muna <bearmuna@gmail.com>
2023-04-28 13:49:56 -0700

    created update function for movie reviews

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection(
 );
 
 router.get('/movies', (req, res) => {
-    db.query('SELECT movie_name, id FROM movies;', (err, data) => {
+    db.query('SELECT movies.id, movies.movie_name, reviews.review FROM reviews RIGHT JOIN movies ON reviews.movie_id = movies.id;', (err, data) => {
         if (err) {
             res.status(500).json({ msg: "Error reading db" });
         } else {
@@ -33,6 +33,29 @@ router.post('/add-movie', (req, res) => {
     })
 });
 
-
+router.put('/update-review', (req, res) => {
+    const movieName = req.body.movie_name;
+    const newReview = req.body.review;
+    let movieID;
+    db.query('SELECT movie_name, id FROM movies;', (err, data) => {
+        if (err) {
+            res.status(500).json({ msg: "Error reading db" });
+        } else {
+            for (let i = 0; i < data.length; i++) {
+                if (movieName == data[i].movie_name) {
+                    movieID = data[i].id;
+                    break;
+                }
+            }
+            db.query('UPDATE reviews SET review = ? WHERE movie_id = ?;', [newReview, movieID], (err ,data) => {
+                if (err) {
+                    res.status(500).json({ msg: "Error updating db" });
+                } else {
+                    res.json({ msg: "Successfully updated movie review!" });
+                }
+            })
+        }
+    })
+})
 
 module.exports = router;
--- db/query.sql
+++ db/query.sql
@@ -15,3 +15,7 @@ SELECT movies.movie_name, reviews.review
 FROM reviews
 JOIN movies
 ON reviews.movie_id = movies.id;
+
+-- SELECT movie_name FROM movies;
+
+-- INSERT INTO movies (movie_name) VALUES (?);
